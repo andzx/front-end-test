9af4687b263ce5060941416509ca92a5820c8478
andzx <andris.greidans@gmail.com>
2020-10-01 18:38:42 +0200

    Created header style and added fonts to the project

--- test-app/src/components/index/index.jsx
+++ test-app/src/components/index/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Index() {
+    // Data
+    const [projects, setProject] = useState([]);
+
+    // Controls
+    useEffect(() => {
+        // Get the page contents
+    }, []);
+
     // View
     return (
-        <div>Index</div>
+        <>
+            <header class="header-index">
+                <h1>Master the front-<span class="text-behind">end</span> w<span class="text-behind">i</span>th fine<span class="text-behind">sse</span></h1>
+                <p>The assignment is supposed to be executed as simple as 
+                possible. Code the template in HTML/CSS and try to avoid 3. 
+                party frameworks as much as possible. Creating the page with 
+                React components will grant extra megamate bonus points.</p>
+                <p class="arrow-bottom color-primary">&#11022;</p>
+                <div class="line line-one line-narrow bg-primary"></div>
+                <div class="line line-two line-narrow bg-primary"></div>
+                <div class="line line-three line-thick bg-primary"></div>
+            </header>
+            <div class="projects">x</div>
+        </>
     );
 }
 
