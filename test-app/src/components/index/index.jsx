a513b4e236f89996b7b7d35c84f0120887acf6a6
andzx <andris.greidans@gmail.com>
2020-10-02 12:40:18 +0200

    Added styling for a project and adjusted header text sizing

--- test-app/src/components/index/index.jsx
+++ test-app/src/components/index/index.jsx
@@ -2,11 +2,21 @@ import React, {useEffect, useState} from 'react';
 
 function Index() {
     // Data
-    const [projects, setProject] = useState([]);
+    const [projects, setProjects] = useState([]);
 
     // Controls
     useEffect(() => {
         // Get the page contents
+    fetch("https://raw.githubusercontent.com/rommel-dk/front-end-test/master/assignment-assets/data.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+            setProjects(result.tiles);
+        },
+
+        (error) => {
+
+        });
     }, []);
 
     // View
@@ -18,12 +28,19 @@ function Index() {
                 possible. Code the template in HTML/CSS and try to avoid 3. 
                 party frameworks as much as possible. Creating the page with 
                 React components will grant extra megamate bonus points.</p>
-                <p class="arrow-bottom color-primary">&#11022;</p>
-                <div class="line line-one line-narrow bg-primary"></div>
-                <div class="line line-two line-narrow bg-primary"></div>
+                <p class="arrow-downwards color-primary">&#11022;</p>
+                <div class="line line-one line-thin bg-primary"></div>
+                <div class="line line-two line-thin bg-primary"></div>
                 <div class="line line-three line-thick bg-primary"></div>
             </header>
-            <div class="projects">x</div>
+            <div class="projects" id="#tiles">
+                <div class="project alt">
+                    <img title="I Imagined that Things Were Speaking" src="https://raw.githubusercontent.com/rommel-dk/front-end-test/master/assignment-assets/images/tiles-image-1.jpg" alt="I Imagined that Things Were Speaking"/>
+                    <div class="date">Project 29 August 2019</div>
+                    <div class="author color-alt">Jams Bonham</div>
+                    <div class="title">I Imagined that Things Were Speaking</div>
+                </div>
+            </div>
         </>
     );
 }
